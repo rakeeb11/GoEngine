ee0acdcefe7caa78dbe419a38210883ab0009fb9
Rakeeb Rajbhandari <rakeeb@Rakeebs-MacBook-Pro.local>
2017-09-17 20:31:28 +0545

    * revert back to mysql driver

--- main/SqlConnection.go
+++ main/SqlConnection.go
@@ -5,11 +5,12 @@ import (
 	"os"
 	"log"
 	"net/http"
-	//"database/sql"
+	"database/sql"
 	"fmt"
-	//"bytes"
-	"github.com/siddontang/go-mysql/client"
+	"bytes"
+	//"github.com/siddontang/go-mysql/client"
 	//"context"
+	//"github.com/siddontang/go-mysql/client"
 )
 
 func main() {
@@ -37,49 +38,49 @@ func handler(w http.ResponseWriter, r *http.Request)  {
 	user := mustGetEnv(SQL_USER)
 	password := mustGetEnv(SQL_PWD)
 
-	conn, err := client.Connect(fmt.Sprintf("%s:%s@cloudsql(%s)/", user, password, connectionName), user, password, "outlet")
-	if err != nil {
-		http.Error(w, fmt.Sprintf("Could not open db: %v", err), 500)
-	}
-	conn.Ping()
-
-	// query databases
-	result, err := conn.Execute("SHOW DATABASES")
-	if err != nil {
-		http.Error(w, fmt.Sprintf("Could not open db: %v", err), 500)
-	}
-	log.Println(result)
-
-
-
-	//w.Header().Set("Content-Type", "text/plain")
-
-	// connect to db
-	//db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@cloudsql(%s)/", user, password, connectionName))
+	//conn, err := client.Connect(fmt.Sprintf("%s:%s@cloudsql(%s)/", user, password, connectionName), user, password, "outlet")
 	//if err != nil {
 	//	http.Error(w, fmt.Sprintf("Could not open db: %v", err), 500)
 	//}
-	//defer db.Close()
+	//conn.Ping()
+
+	// query databases
+	//result, err := conn.Execute("SHOW DATABASES")
+	//if err != nil {
+	//	http.Error(w, fmt.Sprintf("Could not open db: %v", err), 500)
+	//}
+	//log.Println(result)
+
+
+
+	w.Header().Set("Content-Type", "text/plain")
+
+	// connect to db
+	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@cloudsql(%s)/", user, password, connectionName))
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Could not open db: %v", err), 500)
+	}
+	defer db.Close()
 
 	// query all database
-	//rows, err := db.Query("SHOW DATABASES")
-	//if err != nil {
-	//	http.Error(w, fmt.Sprintf("Could not query db: %v", err), 500)
-	//	return
-	//}
-	//defer rows.Close()
+	rows, err := db.Query("SHOW DATABASES")
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Could not query db: %v", err), 500)
+		return
+	}
+	defer rows.Close()
 
 	// print database
-	//buf := bytes.NewBufferString("Databases:\n")
-	//for result.Next() {
-	//	var dbName string
-	//	if err := rows.Scan(&dbName); err != nil {
-	//		http.Error(w, fmt.Sprintf("Could not scan result: %v", err), 500)
-	//		return
-	//	}
-	//	fmt.Fprintf(buf, "- %s\n", dbName)
-	//}
-	//w.Write(buf.Bytes())
+	buf := bytes.NewBufferString("Databases:\n")
+	for rows.Next() {
+		var dbName string
+		if err := rows.Scan(&dbName); err != nil {
+			http.Error(w, fmt.Sprintf("Could not scan result: %v", err), 500)
+			return
+		}
+		fmt.Fprintf(buf, "- %s\n", dbName)
+	}
+	w.Write(buf.Bytes())
 }
 
 func mustGetEnv(key string) (env string) {
